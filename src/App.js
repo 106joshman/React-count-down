79ae61fc392c9ee4a523322fa0446aac8c87eb81
ejembi joshua <ejembijoshman@gmail.com>
2022-07-26 11:24:15 +0100

    taken message icon behind the chat ui when initialized

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className="App" onClick={(e) => e.stopPropagation()}>
       <Header />
       MY CHAT-BOX
-      {show && <Chatbot />}
+      {show && <Chatbot show={show} close={()=>setShow(false)}/>}
       <div className="message_icon w-[60px] h-[60px] bg-purple-500 rounded-full grid place-items-center absolute bottom-0 right-0 mr-5 mb-5 cursor-pointer bounce">
-        <button className="" onClick={handleShow}>
+        <button className="relative" onClick={handleShow}>
           <FiMessageSquare size={32} color="white" />
         </button>
       </div>
--- src/chat-bot/index.jsx
+++ src/chat-bot/index.jsx
@@ -1,38 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import image from "../assets/bot.png";
 import { FaRegPaperPlane } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
 
-const Chatbot = () => {
+const Chatbot = (props) => {
+  const [chat, setChat] = useState("");
+  //   const handleChange = (e) => {
+  //     setChat(e.target.value);
+  //   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(chat);
+  };
+
   return (
     <>
-      <div className="wrapper border rounded-lg bottom-0 right-0 absolute w-[350px] h-[550px] md:h-[400px] p-3 mr-5 mb-5 mx-auto bg-[#07120d]">
+      <div
+        className="wrapper border rounded-lg bottom-0 right-0 absolute z-10 w-[400px] h-[400px] md:h-[400px] p-3 mr-5 mb-5 mx-auto bg-[#07120d]"
+        onClick={(e) => e.stopPropagation()}
+      >
         <header>
-          <div className="head flex flex-row">
-            <img
-              src={image}
-              alt=""
-              srcset=""
-              className="w-[50px] h-[50px] rounded-full"
-            />
-            <div className="right text-white ml-2">
-              <div className="description_name font-semibold">ChatBot.</div>
-              <div className="description_name text-green-500">Happy</div>
+          <div className="head flex flex-row justify-between">
+            <div className="left flex flex-row">
+              <img
+                src={image}
+                alt=""
+                srcset=""
+                className="w-[50px] h-[50px] rounded-full"
+              />
+              <div className="right text-white ml-2">
+                <div className="description_name font-semibold">ChatBot.</div>
+                <div className="description_name text-green-500">Happy</div>
+              </div>
+            </div>
+            <div className="close_bttn ">
+              <button className="text-white" onClick={props.close}>
+                <AiOutlineClose size={32} />
+              </button>
             </div>
           </div>
         </header>
         <div className="chat_wrapper">
-          <div className="text_field flex flex-row items-center bottom-0 absolute mb-3 justify-between w-[95%]">
-            <input
-              type="text"
-              name="message"
-              id="mssg"
-              className=" h-[50px] border-none outline-none p-3 bg-[#edeced] rounded-md w-[75%]"
-              placeholder="Enter your message here"
-            />
-            <div className="bttn items-center">
-              <button className="flex justify-center place-items-center text-white bg-[#363062] border-none outline-none font-semibold text-base rounded-md h-[50px] w-[70px]"><FaRegPaperPlane />Send</button>
+          <form action="" onSubmit={handleSubmit}>
+            <div className="text_field flex flex-row items-center bottom-0 absolute mb-3 justify-between w-[95%]">
+              <input
+                type="text"
+                name="message"
+                id="mssg"
+                value={chat}
+                onChange={(e) => setChat(e.target.value)}
+                className=" h-[50px] border-none outline-none p-3 bg-[#edeced] rounded-md w-[75%]"
+                placeholder="Enter your message here"
+              />
+              <div className="bttn items-center flex justify-evenly place-items-center text-white bg-[#363062] border-none outline-none font-semibold text-base rounded-md h-[50px] w-[80px]">
+                <FaRegPaperPlane />
+                <input
+                  type="submit"
+                  value="Send"
+                />
+              </div>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </>
