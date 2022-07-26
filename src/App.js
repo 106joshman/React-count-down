39e90ba7e5fc8d8d412191b2ba696c779114ed02
ejembi joshua <ejembijoshman@gmail.com>
2022-07-26 03:35:21 +0100

    chat icon toggle fixed

--- src/App.js
+++ src/App.js
@@ -6,22 +6,17 @@ import Chatbot from "./chat-bot";
 function App() {
   const [show, setShow] = useState(false);
 
-  // const handleShow = (e) => {
-  //   if (show) {
-  //     setShow(true);
-  //   }
-    // else {
-    //   setShow(!show);
-    // }
-  // };
-  
+  const handleShow = () => {
+    setShow(!show);
+  };
+
   return (
     <div className="App">
       <Header />
       MY CHAT-BOX
       {show && <Chatbot />}
       <div className="message_icon w-[60px] h-[60px] bg-purple-500 rounded-full grid place-items-center absolute bottom-0 right-0 mr-5 mb-5 cursor-pointer bounce">
-        <button className="" onClick={(e) => setShow(true)}>
+        <button className="" onClick={handleShow}>
           <FiMessageSquare size={32} color="white" />
         </button>
       </div>
--- src/chat-bot/index.jsx
+++ src/chat-bot/index.jsx
@@ -5,7 +5,7 @@ import { FaRegPaperPlane } from "react-icons/fa";
 const Chatbot = () => {
   return (
     <>
-      <div className="wrapper border rounded-lg bottom-0 right-0 absolute w-[350px] h-[400px] p-3 mr-5 mb-5 bg-[#07120d]">
+      <div className="wrapper border rounded-lg bottom-0 right-0 absolute w-[350px] h-[550px] md:h-[400px] p-3 mr-5 mb-5 mx-auto bg-[#07120d]">
         <header>
           <div className="head flex flex-row">
             <img
@@ -14,23 +14,23 @@ const Chatbot = () => {
               srcset=""
               className="w-[50px] h-[50px] rounded-full"
             />
-            <div className="right text-white">
-              <div className="description_name">ChatBot.</div>
-              <div className="description_name">Happy</div>
+            <div className="right text-white ml-2">
+              <div className="description_name font-semibold">ChatBot.</div>
+              <div className="description_name text-green-500">Happy</div>
             </div>
           </div>
         </header>
         <div className="chat_wrapper">
-          <div className="text_field flex flex-row items-center p-1 h-[50px] bottom-0 absolute mb-3 justify-around bg-[#edeced] rounded-md w-[90%]">
+          <div className="text_field flex flex-row items-center bottom-0 absolute mb-3 justify-between w-[95%]">
             <input
               type="text"
               name="message"
               id="mssg"
-              className=" border-none outline-none bg-transparent"
+              className=" h-[50px] border-none outline-none p-3 bg-[#edeced] rounded-md w-[75%]"
               placeholder="Enter your message here"
             />
-            <div className="bttn items-center ">
-              <button className="flex place-items-center text-white bg-[#363062] border-none outline-none rounded-md font-semibold text-base h-[40px] w-[70px]"><FaRegPaperPlane />Send</button>
+            <div className="bttn items-center">
+              <button className="flex justify-center place-items-center text-white bg-[#363062] border-none outline-none font-semibold text-base rounded-md h-[50px] w-[70px]"><FaRegPaperPlane />Send</button>
             </div>
           </div>
         </div>
